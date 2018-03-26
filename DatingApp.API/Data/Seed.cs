c1c8ae9964034f61b69943880967b7381d76eacd
Ramteen85 <ramteen.taheri@gmail.com>
2018-03-27 08:27:45 +1100

    extended the API

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedUsers()
+        {
+            // _context.Users.RemoveRange(_context.Users); //removes users in the database
+            // _context.SaveChanges();
+
+            //seed users
+
+            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<User>>(userData);
+            foreach(var user in users) {
+                //create the password hash and salt
+                byte[] passwordHash, passwordSalt;
+                createPasswordHash("password", out passwordHash, out passwordSalt);
+
+                //populate user details
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.Username = user.Username.ToLower();
+
+                //add user to data context
+                _context.Users.Add(user);
+            }
+
+            //save changes to data context
+            _context.SaveChanges();
+
+        }
+
+        private void createPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            };
+        }
+    }
+}
